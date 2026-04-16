3c9df4df84aa9944d8529dbcb74adb903697c058
baloriagopesh <baloriagopesh8@gmail.com>
2026-04-16 22:59:25 +0530

    Implement getUserCity function
    Add function to retrieve user's city from address

---
+++ app.js
@@ -0,0 +1,14 @@
+function getUserCity(user) {
+    if (user.address.city) {
+        return user.address.city;
+    }
+    return "Unknown";
+}
+
+// Example usage
+const user = {
+    name: "Gopesh",
+    address: null
+};
+
+console.log(getUserCity(user));
